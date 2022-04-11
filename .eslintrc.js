1d73246c6b5bcbdca04783a0735e1c9bcdc326d2
Antonio Villagra De La Cruz <5470315+AntonioVdlC@users.noreply.github.com>
2022-04-11 12:04:25 +0200

    Remove eslint jest plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,12 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
-    "jest/globals": true,
   },
   extends: ["eslint:recommended", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["jest"],
   rules: {
     "no-console": "error",
   },
